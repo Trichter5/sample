1003fb984a29674e037bab6ca5695e7c69af9d51
trichter5 <tobias.froehle@gmx.de>
2020-02-06 16:35:01 +0100

    Fixed button bug

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ var button = document.getElementById('theButton');
 
 //add event listener
 button.addEventListener('click', function(event) {
-  alort("You are awesome!");
+  alart("You are awesome!");
 });
